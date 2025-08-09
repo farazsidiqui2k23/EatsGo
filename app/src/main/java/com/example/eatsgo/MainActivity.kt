3e0797724c4ba43c97de8efea32b3fc27272daa7
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-09 10:21:06 +0500

    mainactivity

--- app/src/main/java/com/example/eatsgo/MainActivity.kt
+++ app/src/main/java/com/example/eatsgo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.eatsgo
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,9 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.eatsgo.ui.theme.EatsGoTheme
 import com.example.eatsgo.ui_screens.BoardingScr
 import com.example.eatsgo.ui_screens.Welcome_scr
+import com.example.eatsgo.ui_screens.auth_screens.Fingerprint_Scr
+import com.example.eatsgo.ui_screens.auth_screens.ForgetPassword_Scr
 import com.example.eatsgo.ui_screens.auth_screens.Login_Scr
 import com.example.eatsgo.ui_screens.auth_screens.SignUp_Scr
 
@@ -27,7 +33,9 @@ class MainActivity : ComponentActivity() {
 //                    Welcome_scr(modifier = Modifier.padding(innerPadding))
 //                    BoardingScr(modifier = Modifier.padding(innerPadding))
 //                    Login_Scr(modifier = Modifier.padding(innerPadding))
-                    SignUp_Scr(modifier = Modifier.padding(innerPadding))
+//                    SignUp_Scr(modifier = Modifier.padding(innerPadding))
+//                    ForgetPassword_Scr(modifier = Modifier.padding(innerPadding))
+//                    Fingerprint_Scr(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -35,11 +43,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App(modifier: Modifier = Modifier, context: Context) {
+
+
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "WelcomeScreen"){
+        composable("WelcomeScreen"){
+
+        }
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+        composable("WelcomeScreen"){}
+
+
+    }
+
 }
 
 @Preview(showBackground = true)
