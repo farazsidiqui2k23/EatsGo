2bc9b6b101a9af384137894c922e24d55da37afa
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-10 21:52:49 +0500

    mainactivity

--- app/src/main/java/com/example/eatsgo/MainActivity.kt
+++ app/src/main/java/com/example/eatsgo/MainActivity.kt
@@ -8,20 +8,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.eatsgo.ui.theme.EatsGoTheme
-import com.example.eatsgo.ui_screens.BoardingScr
 import com.example.eatsgo.ui_screens.Welcome_scr
-import com.example.eatsgo.ui_screens.auth_screens.Fingerprint_Scr
-import com.example.eatsgo.ui_screens.auth_screens.ForgetPassword_Scr
-import com.example.eatsgo.ui_screens.auth_screens.Login_Scr
-import com.example.eatsgo.ui_screens.auth_screens.SignUp_Scr
+import com.example.eatsgo.ui_screens.auth_screens.LoginUI
+import com.example.eatsgo.ui_screens.auth_screens.SignUpScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,32 +27,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EatsGoTheme {
+
+
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Welcome_scr(modifier = Modifier.padding(innerPadding))
-//                    BoardingScr(modifier = Modifier.padding(innerPadding))
-//                    Login_Scr(modifier = Modifier.padding(innerPadding))
-//                    SignUp_Scr(modifier = Modifier.padding(innerPadding))
-//                    ForgetPassword_Scr(modifier = Modifier.padding(innerPadding))
-//                    Fingerprint_Scr(modifier = Modifier.padding(innerPadding))
+                    EatsGo(modifier = Modifier.padding(innerPadding), this)
                 }
+
+
+//                val navController = rememberNavController()
+
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+////                    Welcome_scr(modifier = Modifier.padding(innerPadding))
+////                    BoardingScr(modifier = Modifier.padding(innerPadding))
+////                    LoginUI(this, modifier = Modifier.padding(innerPadding))
+//SignUpScreen(this, navController, modifier = Modifier.padding(innerPadding))
+////                    ForgetPassword_Scr(modifier = Modifier.padding(innerPadding))
+////                    Fingerprint_Scr(modifier = Modifier.padding(innerPadding))
+//                }
+
+
+
             }
         }
     }
 }
 
 @Composable
-fun App(modifier: Modifier = Modifier, context: Context) {
-
-
+fun EatsGo(modifier: Modifier = Modifier, context: Context) {
 
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "WelcomeScreen"){
         composable("WelcomeScreen"){
+Welcome_scr(navController, modifier)
+        }
+        composable("SignUpScreen"){
 
         }
-        composable("WelcomeScreen"){}
-        composable("WelcomeScreen"){}
+        composable("LogInScreen"){
+
+        }
         composable("WelcomeScreen"){}
         composable("WelcomeScreen"){}
         composable("WelcomeScreen"){}
@@ -73,6 +86,8 @@ fun App(modifier: Modifier = Modifier, context: Context) {
 @Composable
 fun GreetingPreview() {
     EatsGoTheme {
-        Greeting("Android")
+        EatsGo(
+            context = LocalContext.current
+        )
     }
 }
