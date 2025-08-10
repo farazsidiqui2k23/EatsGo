50c3165ed4192508ca237178f0a717d932791cf0
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-10 20:44:32 +0500

    improve ui

--- app/src/main/java/com/example/eatsgo/ui_screens/auth_screens/LoginScreen.kt
+++ app/src/main/java/com/example/eatsgo/ui_screens/auth_screens/LoginScreen.kt
@@ -3,6 +3,8 @@
 package com.example.eatsgo.ui_screens.auth_screens
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,7 +16,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.isImeVisible
@@ -24,11 +25,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.input.OutputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Fingerprint
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -44,20 +43,17 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -71,25 +67,21 @@ import com.example.eatsgo.ui.theme.Cream
 import com.example.eatsgo.ui.theme.Orange2
 import com.example.eatsgo.ui.theme.OrangeBase
 import com.example.eatsgo.ui.theme.Yellow2
-import com.example.eatsgo.ui.theme.YellowBase
-import com.example.eatsgo.ui_screens.MainLayout
+import com.example.eatsgo.ui_screens.MainUILayout
 import com.example.eatsgo.ui_screens.SenhaOutputTransformation
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun Login_Scr(modifier: Modifier = Modifier) {
+fun LoginUI(context: Context, modifier: Modifier = Modifier) {
 
-    MainLayout(
+    MainUILayout(
         modifier = modifier,
         title = "Log In",
         changeTitle = "Hello",
         content = { LoginCardLayout(modifier) }
     ) {
-
+        //back press functionality
+        Toast.makeText(context, "Back Pressed", Toast.LENGTH_SHORT).show()
     }
 
 }
@@ -284,7 +276,6 @@ fun LoginCardLayout(modifier: Modifier = Modifier) {
                     fontFamily = FontFamily(Font(R.font.poppins_medium)),
                     color = OrangeBase,
                     fontWeight = FontWeight.Thin,
-
                     )
             }
         }
