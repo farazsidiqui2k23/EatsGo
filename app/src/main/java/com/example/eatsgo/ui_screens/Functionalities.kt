a62ef52ce755de9c54d88bdb70c7575f4c53c3f7
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-10 21:53:40 +0500

    hidepassword class

--- app/src/main/java/com/example/eatsgo/ui_screens/Functionalities.kt
+++ app/src/main/java/com/example/eatsgo/ui_screens/Functionalities.kt
@@ -1,7 +1,11 @@
 package com.example.eatsgo.ui_screens
 
+import androidx.compose.foundation.text.input.OutputTransformation
+import androidx.compose.foundation.text.input.TextFieldBuffer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType.Companion.Text
@@ -21,4 +25,17 @@ fun inputTitle(title: String) {
         fontFamily = FontFamily(Font(R.font.poppins_medium)),
         color = Brown
     )
+}
+
+
+@Stable
+class SenhaOutputTransformation : OutputTransformation {
+    companion object {
+        private val tag = SenhaOutputTransformation::class.simpleName
+        private const val MASCARA_INTERNA = '\u2022'
+    }
+    override fun TextFieldBuffer.transformOutput() {
+        val maskedText = AnnotatedString(MASCARA_INTERNA.toString().repeat(asCharSequence().length))
+        replace(0, length, maskedText)
+    }
 }
