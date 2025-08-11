225e3d920c6889ad4d384ad08d5e2045ed6875b7
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-12 03:35:35 +0500

    Authentication VM

--- app/src/main/java/com/example/eatsgo/Firebase/authViewModel.kt
+++ app/src/main/java/com/example/eatsgo/Firebase/authViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class AuthViewModel : ViewModel() {
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -23,8 +24,8 @@ class AuthViewModel : ViewModel() {
         }
     }
 
-    fun UserLogIn(email:String, password:String){
-        if (email.isBlank() && password.isBlank()){
+    fun UserLogIn(email: String, password: String) {
+        if (email.isBlank() && password.isBlank()) {
             _authState.value = AuthState.onFailure("Missing fields")
         }
 
@@ -32,29 +33,48 @@ class AuthViewModel : ViewModel() {
 
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if(task.isSuccessful){
+                if (task.isSuccessful) {
                     _authState.value = AuthState.Authenticated
-                }else{
-                    _authState.value = AuthState.onFailure(task.exception?.message ?: "something went wrong")
+                } else {
+                    _authState.value =
+                        AuthState.onFailure(task.exception?.message ?: "something went wrong")
                 }
             }
     }
 
-    fun UserSignUp(email: String, password: String){
-        if (email.isBlank() && password.isBlank()){
+    fun UserSignUp(email: String, password: String, name: String, contact: String, dob: String) {
+        if (email.isBlank() && password.isBlank() && name.isBlank() && contact.isBlank() && dob.isBlank()) {
             _authState.value = AuthState.onFailure("Missing fields")
+        }
+        if (!email.contains("@") && !email.contains(".")) {
+            _authState.value = AuthState.onFailure("Invalid email")
+        }
+        if (contact.length != 11) {
+            _authState.value = AuthState.onFailure("Invalid contact number")
+        }
+        if (password.length != 6) {
+            _authState.value = AuthState.onFailure("Password should be 6 characters long")
         }
 
         _authState.value = AuthState.onLoading
 
-        auth.signInWithEmailAndPassword(email, password)
+        auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if(task.isSuccessful){
+
+                if (task.isSuccessful) {
+                    val userId = auth.currentUser?.uid
+                    println(userId)
                     _authState.value = AuthState.Authenticated
-                }else{
-                    _authState.value = AuthState.onFailure(task.exception?.message ?: "something went wrong")
+                } else {
+                    _authState.value =
+                        AuthState.onFailure(task.exception?.message ?: "something went wrong")
                 }
             }
+    }
+
+    fun UserSignOut(){
+        auth.signOut()
+        _authState.value = AuthState.Unauthenticated
     }
 }
 
