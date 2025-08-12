2c51d4a517744b3d9c79cec7eec45b2e8ba61286
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-13 02:23:51 +0500

    improve error msg handling

--- app/src/main/java/com/example/eatsgo/Firebase/authViewModel.kt
+++ app/src/main/java/com/example/eatsgo/Firebase/authViewModel.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class AuthViewModel : ViewModel() {
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    val database = FirebaseDatabase.getInstance().reference.child("users")
 
-    val _authState = MutableLiveData<AuthState>()
-    val authState: LiveData<AuthState> = _authState
+    val _authState = MutableLiveData<AuthState<Nothing>>()
+    val authState: LiveData<AuthState<Nothing>> = _authState
 
     init {
         checkUserStatus()
@@ -26,7 +28,7 @@ class AuthViewModel : ViewModel() {
     fun UserLogIn(email: String, password: String) {
         println("going to check")
         if (email.isBlank() && password.isBlank()) {
-            _authState.value = AuthState.onFailure("Missing fields")
+            _authState.value = AuthState.onFailure(Event("Missing Field"))
         }
         else{
             _authState.value = AuthState.onLoading
@@ -37,7 +39,7 @@ class AuthViewModel : ViewModel() {
                         _authState.value = AuthState.Authenticated
                     } else {
                         _authState.value =
-                            AuthState.onFailure(task.exception?.message ?: "something went wrong")
+                            AuthState.onFailure((Event(task.exception?.message ?: "something went wrong")))
                     }
                 }
         }
@@ -45,33 +47,55 @@ class AuthViewModel : ViewModel() {
 
     fun UserSignUp(email: String, password: String, name: String, contact: String, dob: String) {
 
-        if (email.isBlank() && password.isBlank() && name.isBlank() && contact.isBlank() && dob.isBlank()) {
-            _authState.value = AuthState.onFailure("Missing fields")
+        if (email.isBlank() || password.isBlank() || name.isBlank() || contact.isBlank() || dob.isBlank()) {
+
+            _authState.value = AuthState.onFailure(Event("Missing fields"))
+            println("Missing fields")
         }
-        if (!email.contains("@") && !email.contains(".")) {
-            _authState.value = AuthState.onFailure("Invalid email")
+        else if (!email.contains("@") || !email.contains(".")) {
+
+            _authState.value = AuthState.onFailure(Event("Invalid email"))
+            println("Invalid mails")
         }
-        if (contact.length != 11) {
-            _authState.value = AuthState.onFailure("Invalid contact number")
+        else if (contact.length != 11) {
+            _authState.value = AuthState.onFailure(Event("Invalid contact number"))
+            println("Missing mob")
         }
-        if (password.length != 6) {
-            _authState.value = AuthState.onFailure("Password should be 6 characters long")
+        else if (password.length != 6) {
+            _authState.value = AuthState.onFailure(Event("Password should be 6 characters long"))
+            println("Missing pass")
         }
 
-        _authState.value = AuthState.onLoading
+        else{
+            _authState.value = AuthState.onLoading
 
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
 
-                if (task.isSuccessful) {
-                    val userId = auth.currentUser?.uid
-                    println(userId)
-                    _authState.value = AuthState.Authenticated
-                } else {
-                    _authState.value =
-                        AuthState.onFailure(task.exception?.message ?: "something went wrong")
+                    if (task.isSuccessful) {
+                        val userId = auth.currentUser?.uid
+                        val UserData = userData(
+                            userId = userId.toString(),
+                            name = name,
+                            contact = contact,
+                            dob = dob
+                        )
+
+                        database.child(userId.toString()).setValue(UserData).addOnSuccessListener {
+                            _authState.value = AuthState.Authenticated
+                        }.addOnFailureListener {
+                            _authState.value =
+                                AuthState.onFailure(
+                                    Event(task.exception?.message ?: "something went wrong")
+                                )
+                        }
+
+                    } else {
+                        _authState.value =
+                            AuthState.onFailure(Event(task.exception?.message ?: "something went wrong"))
+                    }
                 }
-            }
+        }
     }
 
     fun UserSignOut(){
@@ -79,21 +103,33 @@ class AuthViewModel : ViewModel() {
         _authState.value = AuthState.Unauthenticated
     }
 
-    fun userData(){
-        val userId : String
-        val name : String
-        val contact : String
-        val dob : String
+}
+
+sealed class AuthState<T> {
+    object Authenticated : AuthState<Nothing>()
+    object Unauthenticated : AuthState<Nothing>()
+    object onLoading : AuthState<Nothing>()
+    data class onFailure(val message: Event<String>) : AuthState<Nothing>()
+}
+
+data class userData(
+    val userId : String,
+    val name : String,
+    val contact : String,
+    val dob : String
+        )
+
+class Event<out T>(val content : T){
+    var isObserved = false
+        private set
+
+    fun getContentIfNotObserved() : T?{
+        return if(!isObserved){
+            isObserved = true
+            content
+        }
+        else{
+            null
+        }
     }
 }
-
-sealed class AuthState {
-    object Authenticated : AuthState()
-    object Unauthenticated : AuthState()
-    object onLoading : AuthState()
-    data class onFailure(val message: String) : AuthState()
-}
-
-data class (
-
-        )
