cf6a79695138138fc1a68c05b1261fcd9dbd06a2
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-13 02:24:55 +0500

    alertdialog

---
+++ app/src/main/java/com/example/eatsgo/ui_screens/AlertDialog.kt
@@ -0,0 +1,43 @@
+package com.example.eatsgo.ui_screens
+
+@Composable
+fun AlertDialogExample(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
