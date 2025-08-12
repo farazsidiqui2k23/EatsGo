09f702acae603c858d0de20a6a1e39f28352258b
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-13 02:24:32 +0500

    realtime db dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
