e0c8f6db3718827931041e0e4a76c752e06f9b20
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-20 02:43:31 +0500

    include masterKey

--- app/src/main/java/com/example/eatsgo/api_setup/RetrofitInstance.kt
+++ app/src/main/java/com/example/eatsgo/api_setup/RetrofitInstance.kt
@@ -1,14 +1,28 @@
 package com.example.eatsgo.api_setup
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
     private val base_URL = "https://api.jsonbin.io/v3/"
 
+
+    private const val MASTER_KEY = "\$2a\$10\$zeNX8W1bEQaF19iAzRkp9e.SsKah5sQPPYqyJq0YlgWYA3PRJdDoG"
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("X-Master-Key", MASTER_KEY)
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
     fun getRetrofitInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(base_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
