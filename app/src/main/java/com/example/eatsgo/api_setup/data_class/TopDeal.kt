7ef131c1b9e30f7b7361611f83a31c98400ef2f3
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-08-20 22:49:16 +0500

    improve dataclass

---
+++ app/src/main/java/com/example/eatsgo/api_setup/data_class/TopDeal.kt
@@ -0,0 +1,10 @@
+package com.example.eatsgo.api_setup.data_class
+
+data class TopDeal(
+    val name: String,
+    val pic: String,
+    val item: String,
+    val desc: String,
+    val discount: String,
+    val tagline: String
+)
